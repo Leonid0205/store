df072e37747c5b9b7ff22abcff1fa59a0e812103
Ekaterina <ekaterina@MacBook-Pro-Ekaterina.local>
2025-01-21 11:56:43 +0300

    Initial init

---
+++ components/shared/product/product-price.tsx
@@ -0,0 +1,21 @@
+import { cn } from "@/lib/utils";
+
+const ProductPrice = ({
+  className,
+  value,
+}: {
+  className?: string;
+  value: number;
+}) => {
+  const stringValue = value.toFixed(2);
+  const [intValue, floatValue] = stringValue.split(".");
+  return (
+    <p className={cn("text-2xl", className)}>
+      <span className="text-xs align-super">$</span>
+      {intValue}
+      <span className="text-xs align-super">.{floatValue}</span>
+    </p>
+  );
+};
+
+export default ProductPrice;
